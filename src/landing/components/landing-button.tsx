1b4446bb5bdc3f834d4fb276839f5eb9dc890273
Wreck-X <hemanthkrishnakumar@gmail.com>
2024-10-19 16:55:57 +0530

    add: routing

--- src/landing/components/landing-button.tsx
+++ src/landing/components/landing-button.tsx
@@ -1,9 +1,17 @@
 import '../components/style/landing-button.css'
 
+import { useNavigate } from 'react-router-dom';
+
 const LandingButton: React.FC = () => {
 
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+      navigate('/gallery'); 
+    };
+
     return (
-        <button className="button button--nina button--text-thick button--text-upper button--size-s" data-text="Start">
+        <button className="button button--nina button--text-thick button--text-upper button--size-s" data-text="Start" onClick={handleClick}>
             <span>S</span>
             <span>T</span>
             <span>A</span>
